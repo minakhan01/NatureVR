802d19bf496752d301cda81ee48a11cc39bf86b8
minakhan01 <minakhan01@gmail.com>
2016-06-11 13:14:44 -0400

    added audio detection for breathing - next: audio-input based particle effects

---
+++ Assets/Nature_FX/Scripts/BreathingControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreathingControl : MonoBehaviour {
+
+	GameObject micController;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		micController = GameObject.Find ("MicController");
+		var loudness = micController.GetComponent<MicControl>().loudness;
+		Debug.Log ("loudness: "+loudness);
+		transform.localScale= new Vector3(1+10*loudness,1+10*loudness,1+10*loudness);
+	}
+
+}
